37c6a9d9e6766b51fadee3600fda3b98248e8115
artur.boronat <artur.boronat@gmail.com>
2018-06-29 00:03:07 +0100

    YAMTL solution B

---
+++ solutions/YAMTL_Solution_B/src/main/java/Changes/AttributeChange.java
@@ -0,0 +1,17 @@
+/**
+ */
+package Changes;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Attribute Change</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see Changes.ChangesPackage#getAttributeChange()
+ * @model
+ * @generated
+ */
+public interface AttributeChange extends ElementaryChange {
+} // AttributeChange
