5fb42ac14170c3f4bfdd19b281e8ae862f5a12f2
Antal János Benjamin <antal.janos.benjamin@gmail.com>
2018-11-07 00:01:07 +0100

    Add NMF to Naiad solution reference as nuget package

---
+++ solutions/Naiad/Naiad/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TTC2018.LiveContest;
+
+namespace Naiad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
