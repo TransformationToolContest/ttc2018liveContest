f95db0b6fbbb5aa525b075d613e2fb1189afdc65
Jozsef Marton <marton@db.bme.hu>
2018-12-28 21:44:07 +0100

    SQL: speed up Q2 by computing pure reachability without retaining path.
    Note: cycle detection if skipped in favor of UNION DISTINCT in recursive SQL

--- solutions/SQLSolution/q2.sql
+++ solutions/SQLSolution/q2.sql
@@ -1,18 +1,19 @@
--- transitive closure of friendship-subgraphs defined by comment likes
-WITH RECURSIVE comment_friends_closed(commentid, head_userid, tail_userid, path) AS (
+WITH RECURSIVE comment_friends_closed(commentid, head_userid, tail_userid) AS (
+-- transitive closure (reachability-only, no path is recorded)
+-- of friendship-subgraphs defined by comment likes
+
     -- start with the users that liked a specific comment.
     -- They are the vertices of the projected users graph for a comment
-    SELECT l.commentid, l.userid AS head_userid, l.userid AS tail_userid, ARRAY[l.userid] AS path
+    SELECT l.commentid, l.userid AS head_userid, l.userid AS tail_userid
       FROM likes l
-  UNION ALL
-    SELECT cfc.commentid, cfc.head_userid, f.user2id as tail_userid, array_append(cfc.path, f.user2id)
+  UNION
+    SELECT cfc.commentid, cfc.head_userid, f.user2id as tail_userid
       FROM comment_friends_closed cfc
          , comment_friends f
      WHERE 1=1
         -- join
        AND cfc.tail_userid = f.user1id
        AND cfc.commentid = f.commentid
-       AND NOT cfc.path && ARRAY[f.user2id] -- user2id is not in the path
 )
 , comment_components AS (
     SELECT commentid, tail_userid AS userid, min(head_userid) AS componentid
