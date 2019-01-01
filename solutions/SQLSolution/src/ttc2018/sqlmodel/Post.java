b9782fdcdc6139f86d2ac9718cc29d7ffd12107a
Jozsef Marton <marton@db.bme.hu>
2019-01-01 21:38:54 +0100

    SQL: remove model instance classes

--- solutions/SQLSolution/src/ttc2018/sqlmodel/Post.java
+++
@@ -1,9 +0,0 @@
-package ttc2018.sqlmodel;
-
-import java.util.Date;
-
-public class Post extends Submission {
-    public Post(int id, Date ts, String content, int submitterid) {
-        super(id, ts, content, submitterid);
-    }
-}
--- solutions/SQLSolution/src/ttc2018/sqlmodel/SqlModelBase.java
+++
@@ -1,4 +0,0 @@
-package ttc2018.sqlmodel;
-
-public abstract class SqlModelBase {
-}
