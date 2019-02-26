cc3c66a627df6f7ab9a2cad0d20190c16dfee552
Jozsef Marton <marton@db.bme.hu>
2019-02-27 00:16:38 +0100

    SQL: correct use of SQL timestamp for change processing

--- solutions/SQLSolution/src/ttc2018/sqlmodel/Submission.java
+++ solutions/SQLSolution/src/ttc2018/sqlmodel/Submission.java
@@ -6,14 +6,14 @@ import java.util.Date;
 
 abstract class Submission extends SqlRowBase {
     long id;
-    java.sql.Date ts;
+    java.sql.Timestamp ts;
     String content;
     long submitterid;
 
 
     public Submission(long id, Date ts, String content, long submitterid) {
         this.id = id;
-        this.ts = new java.sql.Date(ts.getTime());
+        this.ts = new java.sql.Timestamp(ts.getTime());
         this.content = content;
         this.submitterid = submitterid;
     }
@@ -21,7 +21,7 @@ abstract class Submission extends SqlRowBase {
     @Override
     public void setForInsert(PreparedStatement insert) throws SQLException {
         insert.setLong(1, id);
-        insert.setDate(2, ts);
+        insert.setTimestamp(2, ts);
         insert.setString(3, content);
         insert.setLong(4, submitterid);
         // should fields be added or removed here, adjust this method in subclasses
