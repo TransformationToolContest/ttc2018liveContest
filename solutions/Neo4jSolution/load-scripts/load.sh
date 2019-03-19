91b347cbe79f9ae421c32507577a1ef45cbc41b2
marci543 <marci543@users.noreply.github.com>
2019-03-19 14:09:41 +0100

    Fix label name

--- solutions/Neo4jSolution/load-scripts/load.sh
+++ solutions/Neo4jSolution/load-scripts/load.sh
@@ -5,8 +5,8 @@ PREFIX=${NEO4J_DATA_DIR}/graph-
 
 $NEO4J_HOME/bin/neo4j-import --into $NEO4J_DB_DIR \
   --id-type=INTEGER \
-  --nodes:Message:Post "${PREFIX}posts${POSTFIX}" \
-  --nodes:Message:Comment "${PREFIX}comments${POSTFIX}" \
+  --nodes:Submission:Post "${PREFIX}posts${POSTFIX}" \
+  --nodes:Submission:Comment "${PREFIX}comments${POSTFIX}" \
   --nodes:User "${PREFIX}users${POSTFIX}" \
   --relationships:FRIEND "${PREFIX}friends${POSTFIX}" \
   --relationships:LIKES "${PREFIX}likes${POSTFIX}" \
