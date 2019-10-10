c329c0ae26ecdd5d5149324de18f1f83832a22a4
marci543 <marci543@users.noreply.github.com>
2019-10-10 17:09:41 +0200

    [GrB] Extract score calculation to method

--- solutions/GraphBLAS/main.cpp
+++ solutions/GraphBLAS/main.cpp
@@ -14,49 +14,24 @@ extern "C" {
 #include "computation_timer.hpp"
 #include "load.h"
 
-int main(int argc, char **argv) {
-    ComputationTimer total_timer{"Q2"};
-
-    BenchmarkParameters parameters = ParseBenchmarkParameters(argc, argv);
-
-    ok(LAGraph_init());
-    GxB_Global_Option_set(GxB_GLOBAL_NTHREADS, parameters.thread_num);
-    std::cout << parameters.thread_num << '/' << omp_get_max_threads() << std::endl;
-
-    Q2_Input input = load("../../models/1024/");
+namespace {
 
     using score_type = std::tuple<uint64_t, time_t, GrB_Index>;
-    std::priority_queue<score_type, std::vector<score_type>, std::greater<>> top_scores;
 
-    // make sure tuples are in row-major order (SuiteSparse extension)
-    GxB_Format_Value format;
-    ok(GxB_Matrix_Option_get(input.likes_matrix_tran, GxB_FORMAT, &format));
-    if (format != GxB_BY_ROW) {
-        throw std::runtime_error{"Matrix is not CSR"};
-    }
+    inline void
+    compute_score_for_comment(const Q2_Input &input, GrB_Index comment_col, GrB_Index *likes_comment_array_first,
+                              GrB_Index *likes_comment_array_last, GrB_Index *likes_user_array_first,
+                              std::priority_queue<score_type, std::vector<score_type>, std::greater<>> &top_scores) __attribute__ ((always_inline));
 
-    std::unique_ptr<GrB_Index[]> likes_trg_comment_columns{new GrB_Index[input.likes_num]},
-            likes_src_user_columns{new GrB_Index[input.likes_num]};
-    GrB_Index *likes_comment_array_first = likes_trg_comment_columns.get(),
-            *likes_comment_array_last = likes_trg_comment_columns.get() + input.likes_num;
-
-    // nullptr to avoid extracting matrix values (SuiteSparse extension)
-    GrB_Index nvals = input.likes_num;
-    ok(GrB_Matrix_extractTuples_BOOL(likes_trg_comment_columns.get(), likes_src_user_columns.get(), nullptr, &nvals,
-                                     input.likes_matrix_tran));
-    assert(nvals == input.likes_num);
-
-    // find tuple sequences of each comment in row-major array
-    for (GrB_Index comment_col = 0; comment_col < input.comments_size(); ++comment_col) {
+    void compute_score_for_comment(const Q2_Input &input, GrB_Index comment_col, GrB_Index *likes_comment_array_first,
+                                   GrB_Index *likes_comment_array_last, GrB_Index *likes_user_array_first,
+                                   std::priority_queue<score_type, std::vector<score_type>, std::greater<>> &top_scores) {
         auto[likes_comment_first, likes_comment_last] = std::equal_range(likes_comment_array_first,
                                                                          likes_comment_array_last, comment_col);
-        if (likes_comment_first == likes_comment_last) {
-            // no likes for this comment
-            continue;
-        } else {
+        if (likes_comment_first != likes_comment_last) {
             GrB_Index likes_count = std::distance(likes_comment_first, likes_comment_last);
             GrB_Index *likes_user_first =
-                    likes_src_user_columns.get() + std::distance(likes_comment_array_first, likes_comment_first);
+                    likes_user_array_first + std::distance(likes_comment_array_first, likes_comment_first);
 
             GrB_Matrix friends_subgraph;
             ok(GrB_Matrix_new(&friends_subgraph, GrB_BOOL, likes_count, likes_count));
@@ -68,8 +43,8 @@ int main(int argc, char **argv) {
             GrB_Vector components_vector = nullptr;
             ok(LAGraph_cc(friends_subgraph, &components_vector));
 
-            GrB_Index nvals_local = input.likes_num;
-            ok(GrB_Vector_nvals(&nvals_local, components_vector));
+            GrB_Index nvals = input.likes_num;
+            ok(GrB_Vector_nvals(&nvals, components_vector));
             assert(nvals == likes_count);
 
             GrB_Index n;
@@ -80,8 +55,8 @@ int main(int argc, char **argv) {
                     component_sizes(likes_count);
 
             // nullptr: SuiteSparse extension
-            nvals_local = likes_count;
-            ok(GrB_Vector_extractTuples_UINT64(nullptr, components.data(), &nvals_local, components_vector));
+            nvals = likes_count;
+            ok(GrB_Vector_extractTuples_UINT64(nullptr, components.data(), &nvals, components_vector));
             assert(nvals == likes_count);
 
             for (auto component_id:components)
@@ -101,6 +76,45 @@ int main(int argc, char **argv) {
             ok(GrB_Matrix_free(&friends_subgraph));
             ok(GrB_Vector_free(&components_vector));
         }
+    }
+}
+
+int main(int argc, char **argv) {
+    ComputationTimer total_timer{"Q2"};
+
+    BenchmarkParameters parameters = ParseBenchmarkParameters(argc, argv);
+
+    ok(LAGraph_init());
+    GxB_Global_Option_set(GxB_GLOBAL_NTHREADS, parameters.thread_num);
+    std::cout << parameters.thread_num << '/' << omp_get_max_threads() << std::endl;
+
+    Q2_Input input = load("../../models/1024/");
+
+    std::priority_queue<score_type, std::vector<score_type>, std::greater<>> top_scores;
+
+    // make sure tuples are in row-major order (SuiteSparse extension)
+    GxB_Format_Value format;
+    ok(GxB_Matrix_Option_get(input.likes_matrix_tran, GxB_FORMAT, &format));
+    if (format != GxB_BY_ROW) {
+        throw std::runtime_error{"Matrix is not CSR"};
+    }
+
+    std::unique_ptr<GrB_Index[]> likes_trg_comment_columns{new GrB_Index[input.likes_num]},
+            likes_src_user_columns{new GrB_Index[input.likes_num]};
+    GrB_Index *likes_comment_array_first = likes_trg_comment_columns.get(),
+            *likes_comment_array_last = likes_trg_comment_columns.get() + input.likes_num,
+            *likes_user_array_first = likes_src_user_columns.get();
+
+    // nullptr to avoid extracting matrix values (SuiteSparse extension)
+    GrB_Index nvals = input.likes_num;
+    ok(GrB_Matrix_extractTuples_BOOL(likes_trg_comment_columns.get(), likes_src_user_columns.get(), nullptr, &nvals,
+                                     input.likes_matrix_tran));
+    assert(nvals == input.likes_num);
+
+    // find tuple sequences of each comment in row-major array
+    for (GrB_Index comment_col = 0; comment_col < input.comments_size(); ++comment_col) {
+        compute_score_for_comment(input, comment_col, likes_comment_array_first, likes_comment_array_last,
+                                  likes_user_array_first, top_scores);
     }
 
     std::vector<score_type> top_scores_vector;
