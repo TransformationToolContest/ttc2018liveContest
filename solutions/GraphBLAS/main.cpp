4968ad5cd8e2013ea4322a5f34a3b35108abddfb
marci543 <marci543@users.noreply.github.com>
2019-10-20 19:22:48 +0200

    [GrB] Choose implementation by tool name

--- solutions/GraphBLAS/main.cpp
+++ solutions/GraphBLAS/main.cpp
@@ -24,16 +24,23 @@ int main(int argc, char **argv) {
     GxB_Global_Option_set(GxB_GLOBAL_NTHREADS, parameters.thread_num);
     // std::cout << parameters.thread_num << '/' << omp_get_max_threads() << std::endl;
 
-    Q2_Solution_Incremental_Per_Comment solution{parameters};
+    std::unique_ptr<Q2_Solution> solution;
 
-    solution.load();
-    solution.initial();
+    if (parameters.Tool == "GBq2-Batch")
+        solution = std::make_unique<Q2_Solution_Batch>(parameters);
+    else if (parameters.Tool == "GBq2-Incr-Comment")
+        solution = std::make_unique<Q2_Solution_Incremental_Per_Comment>(parameters);
+    else
+        throw std::runtime_error{"Unknown tool: " + parameters.Tool};
+
+    solution->load();
+    solution->initial();
 
     for (int iteration = 1; iteration <= parameters.Sequences; ++iteration) {
-        solution.update(iteration);
+        solution->update(iteration);
     }
 
     // Cleanup
-    solution.free();
+    solution->free();
     ok(GrB_finalize());
 }
