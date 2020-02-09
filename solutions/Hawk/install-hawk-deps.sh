01a16232d311b85fc3b01097b3123723c184af17
Jozsef Marton <marton@db.bme.hu>
2020-02-09 08:05:22 +0100

    Change hawk git transport: git:// -> https://
    The original URL returned an error:
    fatal: remote error: access denied or repository not exported: /gitroot/hawk/hawk.git

--- solutions/Hawk/install-hawk-deps.sh
+++ solutions/Hawk/install-hawk-deps.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if ! test -d hawk; then
-  git clone -b v1.2.0-rc1 --depth 1 git://git.eclipse.org/gitroot/hawk/hawk.git
+  git clone -b v1.2.0-rc1 --depth 1 https://git.eclipse.org/r/hawk/hawk.git
 fi
 cd hawk
 mvn -B --quiet -f pom-plain.xml install -DskipTests
