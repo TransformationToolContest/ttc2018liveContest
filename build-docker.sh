a2f8d8e89ef6b5be17cc91f09cc840d4f024f374
Antonio García-Domínguez <a.garcia-dominguez@aston.ac.uk>
2020-07-27 14:44:53 +0100

    Add Dockerfile for .NET Core 3.1 solutions

--- build-docker.sh
+++ build-docker.sh
@@ -12,3 +12,8 @@ sudo docker push "$DOCKER_REPO:java8"
 # Java 11 solutions
 sudo docker build -t "$DOCKER_REPO:java11" -f solutions/Dockerfile-java11 .
 sudo docker push "$DOCKER_REPO:java11"
+
+# .NET Core 3.1 solutions
+
+sudo docker build -t "$DOCKER_REPO:net31" -f solutions/Dockerfile-net31 .
+sudo docker push "$DOCKER_REPO:net31"
