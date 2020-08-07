55bb76864c6a4e51dc4e0a35bc751ec0b27a1420
marci543 <marci543@users.noreply.github.com>
2020-08-07 17:46:30 +0200

    Add options and tools to build-docker.sh

--- build-docker.sh
+++ build-docker.sh
@@ -5,15 +5,40 @@ DOCKER_REPO=ftsrg/ttc2018
 # exit immediately if a command fails
 set -e
 
-# Java 8 solutions
-sudo docker build -t "$DOCKER_REPO:java8" -f solutions/Dockerfile-java8 .
-sudo docker push "$DOCKER_REPO:java8"
+if [[ "$#" -eq 0 ]]; then
+  build=1
 
-# Java 11 solutions
-sudo docker build -t "$DOCKER_REPO:java11" -f solutions/Dockerfile-java11 .
-sudo docker push "$DOCKER_REPO:java11"
+  echo Parameters:
+  echo "  -b|--build (default if none given)"
+  echo "  -p|--push"
+  echo
+fi
 
-# .NET Core 3.1 solutions
+# https://stackoverflow.com/a/33826763
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        -b|--build) build=1 ;;
+        -p|--push) push=1 ;;
+        *) echo "Unknown parameter passed: $1"; exit 1 ;;
+    esac
+    shift
+done
 
-sudo docker build -t "$DOCKER_REPO:net31" -f solutions/Dockerfile-net31 .
-sudo docker push "$DOCKER_REPO:net31"
+# `ls solutions/Dockerfile-* | sed -r 's/.*-(.+$)/\1/' | xargs`
+TOOLS="differential graphblas java11 java8 neo4j net31 sql"
+# where license permits push images to Docker Hub
+TOOLS_TO_PUSH="differential graphblas java11 java8 net31 sql"
+
+if [ $build ]; then
+  for TOOL in $TOOLS; do
+    echo Build "$TOOL"
+    docker build -t "$DOCKER_REPO:$TOOL" -f "solutions/Dockerfile-$TOOL" .
+  done
+fi
+
+if [ $push ]; then
+  for TOOL in $TOOLS_TO_PUSH; do
+    echo Push "$TOOL"
+    docker push "$DOCKER_REPO:$TOOL"
+  done
+fi
