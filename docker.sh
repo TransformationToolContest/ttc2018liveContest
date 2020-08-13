9a5344117ae72c5f1c80113017059778e8fc2a59
marci543 <marci543@users.noreply.github.com>
2020-08-13 17:28:02 +0200

    Add --tags option to docker.sh

--- docker.sh
+++ docker.sh
@@ -10,8 +10,12 @@ if [[ "$#" -eq 0 ]]; then
   echo "  -b|--build"
   echo "  -p|--push"
   echo "  -r|--run"
+  echo
+  echo "  -t|--tags \"TAG1 TAG2 ...\""
   exit
 fi
+
+TAGS=$(docker/ls-images.sh)
 
 # https://stackoverflow.com/a/33826763
 while [[ "$#" -gt 0 ]]; do
@@ -19,31 +23,36 @@ while [[ "$#" -gt 0 ]]; do
         -b|--build) build=1 ;;
         -p|--push) push=1 ;;
         -r|--run) run=1 ;;
+
+        -t|--tags) TAGS="$2"; shift ;;
         *) echo "Unknown parameter passed: $1"; exit 1 ;;
     esac
     shift
 done
 
-TOOLS=$(docker/ls-images.sh)
 # where license permits push images to Docker Hub
-TOOLS_TO_PUSH=$(echo "$TOOLS" | grep -v neo4j)
+TAGS_TO_PUSH=$(echo "$TAGS" | grep -v neo4j)
+
+echo "Operations: ${build+build }${push+push }${run+run }"
+echo Tags: $TAGS
+echo
 
 if [ $build ]; then
-  for TOOL in $TOOLS; do
-    echo "==================== Build $TOOL ===================="
-    docker build -t "$DOCKER_REPO:$TOOL" -f "docker/Dockerfile-$TOOL" .
+  for TAG in $TAGS; do
+    echo "==================== Build $TAG ===================="
+    docker build -t "$DOCKER_REPO:$TAG" -f "docker/Dockerfile-$TAG" .
   done
 fi
 
 if [ $push ]; then
-  for TOOL in $TOOLS_TO_PUSH; do
-    echo "==================== Push $TOOL ===================="
-    docker push "$DOCKER_REPO:$TOOL"
+  for TAG in $TAGS_TO_PUSH; do
+    echo "==================== Push $TAG ===================="
+    docker push "$DOCKER_REPO:$TAG"
   done
 fi
 
 if [ $run ]; then
-  for TOOL in $TOOLS; do
+  for TOOL in $TAGS; do
     echo "==================== Run $TOOL ===================="
     HOST_OUTPUT_PATH=$(realpath output/output-docker-$TOOL.csv)
     TOOL_DOCKER_CONFIG_PATH=$(realpath config/config-docker-$TOOL.json)
