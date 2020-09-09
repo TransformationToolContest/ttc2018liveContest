b84b5fb7d67cee4c3063f638d4c1adf22374acf4
marci543 <marci543@users.noreply.github.com>
2020-09-09 18:20:56 +0200

    Move restart step in SQL load

--- solutions/SQLSolution/load-scripts/load.sh
+++ solutions/SQLSolution/load-scripts/load.sh
@@ -7,15 +7,15 @@ PG_DB_NAME=${PG_DB_NAME:-ttc2018sf1}
 PG_USER=${PG_USER:-$USER}
 PG_PORT=${PG_PORT:-5432}
 
-# check whether run from Docker image
-if [ -f /ttc/entrypoint.sh ]; then
-  # restart DB to avoid any stuck process from previous run
-  service postgresql restart
-  # show status
-  pg_lsclusters
-fi
-
 if [ "$1" == "" -o "$1" == "schema-only" ]; then
+  # check whether run from Docker image
+  if [ -f /ttc/entrypoint.sh ]; then
+    # restart DB to avoid any stuck process from previous run
+    service postgresql restart
+    # show status
+    pg_lsclusters
+  fi
+
   /usr/bin/dropdb --if-exists $PG_DB_NAME -U $PG_USER -p $PG_PORT
   /usr/bin/createdb $PG_DB_NAME -U $PG_USER -p $PG_PORT --template template0 -l "C"
   /usr/bin/psql -d $PG_DB_NAME -U $PG_USER -p $PG_PORT -a -f $(dirname $0)/schema.sql
