76e8fe9469586d846a0f25d97587f3379b1d1835
Jozsef Marton <marton@db.bme.hu>
2021-04-10 09:58:03 +0200

    SQL: remove Comment.rootPost edge from SQL model

--- solutions/SQLSolution/src/ttc2018/sqlmodel/Comment.java
+++ solutions/SQLSolution/src/ttc2018/sqlmodel/Comment.java
@@ -6,18 +6,15 @@ import java.util.Date;
 
 public class Comment extends Submission {
     long parentid;
-    long postid;
 
-    public Comment(long id, Date ts, String content, long submitterid, long parentid, long postid) {
+    public Comment(long id, Date ts, String content, long submitterid, long parentid) {
         super(id, ts, content, submitterid);
 
         this.parentid = parentid;
-        this.postid = postid;
     }
 
     public void setForInsert(PreparedStatement insert) throws SQLException {
         super.setForInsert(insert);
         insert.setLong(5, parentid);
-        insert.setLong(6, postid);
     }
 }
