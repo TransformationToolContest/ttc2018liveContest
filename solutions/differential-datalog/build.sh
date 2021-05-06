34ea72b7039b6fa8315196d43b8e6685e1268bee
marci543 <marci543@users.noreply.github.com>
2021-05-06 02:06:48 +0200

    Refactor build script

--- solutions/differential-datalog/build.sh
+++ solutions/differential-datalog/build.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
+set -Eeuo pipefail
 
-ddlog_solution_dir=$(dirname $(dirname $(realpath $0)))
+TARGET=$1
 
-# Download ddlog v0.39.0 from Github repository.
-export DDLOG_HOME=$ddlog_solution_dir/ddlog
+# Use downloaded ddlog v0.39.0 from Github repository.
+export DDLOG_HOME=$(dirname $(realpath "$0"))/ddlog
 echo DDLOG_HOME is set to $DDLOG_HOME
-
-chmod +x $DDLOG_HOME/bin/ddlog
 echo The location of ddlog binary is $(which $DDLOG_HOME/bin/ddlog)
 
 # For compiled ddlog binary in default path
@@ -14,9 +13,14 @@ echo The location of ddlog binary is $(which $DDLOG_HOME/bin/ddlog)
 # For ddlog compiled from source and change the path if the source code is in a different directory.
 # export DDLOG_HOME=$HOME/differential-datalog
 
+cd "$TARGET"
 # Create runtime for socialnetwork domain
-$DDLOG_HOME/bin/ddlog -i snq1.dl && 
+"$DDLOG_HOME/bin/ddlog" -i "$TARGET.dl"
 # Open the folder and build the runtime in Rust
-(cd snq1_ddlog && cargo build --release) && 
+pushd "${TARGET}_ddlog"
+cargo build --release
+popd
 # Open the folder and build the project that depends on the runtime 
-(cd snq1_lib && cargo build --release) 
+pushd "${TARGET}_lib"
+cargo build --release
+popd
