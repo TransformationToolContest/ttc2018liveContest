a10e9f59324d3a7efd1e1b9471078ffa9519a34d
marci543 <marci543@users.noreply.github.com>
2021-08-23 12:21:48 +0200

    Check availability of Docker Hub image

---
+++ docker/check-docker-hub-tags.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -Eeuo pipefail
+
+if [[ "$#" -le 1 ]]; then
+  echo "Parameters: DOCKER_REPO TAG..."
+  echo "  E.g.: library/ubuntu 18.04 20.04"
+  exit
+fi
+
+DOCKER_REPO="$1"
+shift
+
+# https://stackoverflow.com/a/39641186
+# https://stackoverflow.com/a/68654659
+TOKEN=$(curl -s "https://auth.docker.io/token?service=registry.docker.io&scope=repository:$DOCKER_REPO:pull" | jq -r .token)
+while [[ "$#" -gt 0 ]]; do
+    TAG="$1"
+    MANIFEST_URL="https://index.docker.io/v2/$DOCKER_REPO/manifests/$TAG"
+    { curl --fail --silent --show-error --header "Authorization: Bearer $TOKEN" "$MANIFEST_URL" | jq "{name:.name, tag:.tag}" ; } \
+        || { EXIT_CODE=$? ; echo "Error: $MANIFEST_URL" ; exit $EXIT_CODE ; }
+    shift
+done
