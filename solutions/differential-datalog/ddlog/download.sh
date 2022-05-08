1c43058e768e74b24e682905ac898133ea1b8860
Qishen Zhang <qishen23@gmail.com>
2022-05-07 22:59:39 -0500

    Update DDLog to v1.2.3

--- solutions/differential-datalog/ddlog/download.sh
+++ solutions/differential-datalog/ddlog/download.sh
@@ -6,7 +6,7 @@ cd "$(dirname "$0")"
 if ! sha256sum -c --status checksum.sha256 2>/dev/null; then
     rm -rf ./*/
     echo Downloading ddlog
-    wget -qO- "https://github.com/vmware/differential-datalog/releases/download/v0.39.0/ddlog-v0.39.0-20210411172417-linux.tar.gz" \
+    wget -qO- "https://github.com/vmware/differential-datalog/releases/download/v1.2.3/ddlog-v1.2.3-20211213235218-Linux.tar.gz" \
         | tar -zxC .. ddlog/{bin,lib}/
     sha256sum -c --quiet checksum.sha256
     echo ddlog is downloaded and extracted
